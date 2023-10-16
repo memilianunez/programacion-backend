e12c17d7b979c1af4c290ed97bdbadbad333386c
memilianunez <marianaemilia.ar@gmail.com>
2023-10-16 18:55:20 -0300

    Se crearon carpetas y archivos del desafio 2ç

---
+++ desafio2/main.js
@@ -0,0 +1,28 @@
+const ProductManager = require('./ProductManager');
+
+const productManager = new ProductManager('./productos.json'); 
+
+productManager.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
+console.log(productManager.getProducts());
+
+try {
+    productManager.addProduct('producto prueba', 'Otro producto prueba', 300, 'Sin imagen', 'abc124', 10);
+} catch (error) {
+    console.error(error.message);
+}
+
+try {
+    const product = productManager.getProductById(1); 
+    console.log(product);
+} catch (error) {
+    console.error(error.message);
+}
+
+productManager.updateProduct(1, { price: 250 });
+
+try {
+    productManager.deleteProduct(1);
+    console.log(productManager.getProducts());
+} catch (error) {
+    console.error(error.message);
+}
