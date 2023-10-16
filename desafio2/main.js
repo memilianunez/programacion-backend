676be9b33deb6cfd10ce448a94ace09c61a3149d
memilianunez <marianaemilia.ar@gmail.com>
2023-10-16 19:04:06 -0300

    Mejoras en el código para poder trabajar con múltiples productos y la devolucion de nul

--- desafio2/main.js
+++ desafio2/main.js
@@ -1,3 +1,33 @@
+// const ProductManager = require('./ProductManager');
+
+// const productManager = new ProductManager('./productos.json'); 
+
+// productManager.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
+// console.log(productManager.getProducts());
+
+// try {
+//     productManager.addProduct('producto prueba', 'Otro producto prueba', 300, 'Sin imagen', 'abc124', 10);
+// } catch (error) {
+//     console.error(error.message);
+// }
+
+// try {
+//     const product = productManager.getProductById(1); 
+//     console.log(product);
+// } catch (error) {
+//     console.error(error.message);
+// }
+
+// productManager.updateProduct(1, { price: 250 });
+
+// try {
+//     productManager.deleteProduct(1);
+//     console.log(productManager.getProducts());
+// } catch (error) {
+//     console.error(error.message);
+// }
+
+
 const ProductManager = require('./ProductManager');
 
 const productManager = new ProductManager('./productos.json'); 
@@ -11,18 +41,17 @@ try {
     console.error(error.message);
 }
 
-try {
-    const product = productManager.getProductById(1); 
+const product = productManager.getProductById(1); 
+if (product) {
     console.log(product);
-} catch (error) {
-    console.error(error.message);
+} else {
+    console.log('Producto no encontrado');
 }
 
 productManager.updateProduct(1, { price: 250 });
 
-try {
-    productManager.deleteProduct(1);
+if (productManager.deleteProduct(1)) {
     console.log(productManager.getProducts());
-} catch (error) {
-    console.error(error.message);
+} else {
+    console.log('Producto no encontrado');
 }
