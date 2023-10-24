c19822e81ba9634bdc94a840722eca23e54717da
memilianunez <marianaemilia.ar@gmail.com>
2023-10-24 14:03:23 -0300

    Corrección nombre de productos, texto thumbnail y descripcion se agregó

--- desafio3/src/app.js
+++ desafio3/src/app.js
@@ -19,12 +19,14 @@ app.get('/products', (req, res) => {
 app.get("/products/:pid", async (req, res) => {
     try {
         const productId = parseInt(req.params.pid);
+
+
         const product = productManager.getProductById(productId);
 
         if (product) {
-            res.json({ product });
+        res.json({ productId });
         } else {
-            res.status(404).json({ error: "No se encontró el producto" });
+        res.status(404).json({ error: "No se encontró el producto" });
         }
     } catch (error) {
         console.error("Error al obtener producto por ID:", error);
