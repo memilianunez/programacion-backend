33aea396f8684798ced2d06b941e0cc43f0aaeb9
memilianunez <marianaemilia.ar@gmail.com>
2023-11-07 15:54:57 -0300

    Se intalaron dependencias de socket.io, se creó carpeta view con archivos correspondientes y se modificó el archivo server.js

--- desafio4/server.js
+++ desafio4/server.js
@@ -2,22 +2,40 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.json());
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const exphbs = require('express-handlebars');
 
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
 
 const productsRouter = require('./routes/products');
 app.use('/api/products', productsRouter);
 
-
 const cartsRouter = require('./routes/carts');
 app.use('/api/carts', cartsRouter);
 
-
 app.get('/', (req, res) => {
-    res.send('¡Bienvenidx!'); 
+    res.send('¡Bienvenidx!');
 });
 
 
-app.listen(port, () => {
+app.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts');
+});
+
+
+io.on('connection', (socket) => {
+    console.log('Cliente conectado');
+
+
+    socket.on('updateProductList', () => {
+    });
+});
+
+http.listen(port, () => {
     console.log(`Servidor corriendo correctamente en el puerto ${port}`);
 });
