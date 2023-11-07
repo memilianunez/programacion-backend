54730ab10df9b70b463a8fb8d3fb17712d26b6aa
memilianunez <marianaemilia.ar@gmail.com>
2023-11-07 16:17:06 -0300

    Modificacion en const io

--- desafio4/server.js
+++ desafio4/server.js
@@ -3,9 +3,8 @@ const app = express();
 const port = 8080;
 
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http); 
 const exphbs = require('express-handlebars');
-
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -22,17 +21,17 @@ app.get('/', (req, res) => {
     res.send('¡Bienvenidx!');
 });
 
-
 app.get('/realtimeproducts', (req, res) => {
-    res.render('realTimeProducts');
+    res.render('realTimeProducts', { products: productList });
 });
-
 
 io.on('connection', (socket) => {
     console.log('Cliente conectado');
 
+    socket.emit('updateProductList', productList);
 
     socket.on('updateProductList', () => {
+        io.emit('updateProductList', productList);
     });
 });
 
