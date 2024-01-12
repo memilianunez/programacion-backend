723576577257f24d0311fc29fa3b6e2e319543a9
memilianunez <marianaemilia.ar@gmail.com>
2024-01-11 21:53:38 -0300

    Se creó un nuevo modelo de ususario

---
+++ desafio5/src/daos/mongodb/models/user.model.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['usuario', 'admin'], default: 'usuario' },
+});
+
+const UserModel = model('User', userSchema);
+
+module.exports = UserModel;
