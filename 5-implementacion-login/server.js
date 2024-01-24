88e2aff07ca3db5d748192d948c41b9f4f9ff9cd
memilianunez <marianaemilia.ar@gmail.com>
2024-01-23 21:20:42 -0300

    Renombre de carpetas

--- 5-implementacion-login/server.js
+++ 5-implementacion-login/server.js
@@ -1,34 +1,27 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import handlebars from "express-handlebars";
+import { __dirname } from "./utils.js";
+import loginRouter from "./routes/login.router.js";
+import viewsRouter from "./routes/views.router.js";
 
 const app = express();
 
 const secretKey = "1234";
 
 app.use(cookieParser(secretKey));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/set-cookie", (req, res) => {
-  res.cookie("idioma", "ingles").json({ msg: "ok" });
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
+app.use("/login", loginRouter);
+app.use("/", viewsRouter);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Escuchando al puerto ${PORT}`);
 });
-
-app.get("/getcookie", (req, res) => {
-  console.log(req.cookies);
-  console.log(req.signedCookies);
-  const { idioma } = req.cookies;
-  idioma === "ingles" ? res.send("Hello!") : res.send("Hola!");
-});
-
-app.get("/set2", (req, res) => {
-  res.cookie("saludo", "hola", { maxAge: 3000 }).json({ msg: "ok" });
-});
-
-app.get("/set-signed-cookie", (req, res) => {
-  res.cookie("nombre", "Andres", { signed: true }).json({ msg: "ok" });
-});
-
-app.get("/clear", (req, res) => {
-  res.clearCookie("nombre").json({ msg: "ok" });
-});
-
-app.listen(8080, () => console.log("server ok"));
