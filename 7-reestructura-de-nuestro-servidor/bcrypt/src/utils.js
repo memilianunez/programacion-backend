557ac800f9ad8227a1ce034b4c2fb56b233c2333
memilianunez <marianaemilia.ar@gmail.com>
2024-01-23 23:04:54 -0300

    Desafio 7

--- 7-reestructura-de-nuestro-servidor/bcrypt/src/utils.js
+++ 7-reestructura-de-nuestro-servidor/bcrypt/src/utils.js
@@ -1,25 +1,34 @@
+// import { hashSync, genSaltSync, compareSync } from "bcrypt";
+
+// //Registro:
+// export const createHash = (password) => {
+//   return hashSync(password, genSaltSync(10));
+// };
+
+// //Login
+
+//  * @param {*} password contraseña proporcionada por el usuario, sin hashear
+//  * @param {*} user usuario existente en base de datos
+//  * @returns boolean
+
+// export const isValidPass = (password, user) => {
+//   return compareSync(password, user.password);
+// };
+
+
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 export const __dirname = dirname(fileURLToPath(import.meta.url));
 
-/* ------------------------------------ - ----------------------------------- */
+import bcryptjs from "bcryptjs";
 
-import { hashSync, genSaltSync, compareSync } from "bcrypt";
+export const createHash = (password) =>
+  bcryptjs.hashSync(password, bcryptjs.genSaltSync(10));
 
-//registro
-export const createHash = (password) => {
-  return hashSync(password, genSaltSync(10));
-};
+export const isValidPassword = (user, password) =>
+  bcryptjs.compareSync(password, user.password);
 
-//login
-
-/**
- * 
- * @param {*} password contraseña proporcionada por el usuario, sin hashear
- * @param {*} user usuario existente en base de datos
- * @returns boolean
- */
-
-export const isValidPass = (password, user) => {
-  return compareSync(password, user.password);
+export const createResponse = (res, statusCode, data) => {
+  return res.status(statusCode).json({ data });
 };
