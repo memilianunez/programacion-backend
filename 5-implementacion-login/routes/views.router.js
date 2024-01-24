957d0edd991d54614b8edd30d7810ca87b00afe5
memilianunez <marianaemilia.ar@gmail.com>
2024-01-23 21:57:24 -0300

    Desafío 6

--- 5-implementacion-login/routes/views.router.js
+++ 5-implementacion-login/routes/views.router.js
@@ -3,7 +3,9 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/',(req,res)=>{
-    res.render('login')
+    res.render('login'),
+    res.render('chat')
+
 })
 
 export default router
--- desafio6/src/config/db.config.js
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-export const dbAdmin = process.env.DB_ADMIN;
-export const dbPassword = process.env.DB_PASSWORD;
-export const dbHost = process.env.DB_HOST;
