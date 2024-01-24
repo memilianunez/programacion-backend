0e445877f21919167b0a25a270b25443d24f4b6d
memilianunez <marianaemilia.ar@gmail.com>
2024-01-23 22:51:44 -0300

    Desafio 7

--- 7-reestructura-de-nuestro-servidor/bcrypt/src/controllers/user.controllers.js
+++ 7-reestructura-de-nuestro-servidor/bcrypt/src/controllers/user.controllers.js
@@ -1,25 +1,47 @@
-import UserDao from "../daos/user.dao.js";
-const userDao = new UserDao();
+import Controllers from "./class.controller.js";
+import UserService from "../services/user.services.js";
+import { createResponse } from "../utils.js";
+const userService = new UserService();
 
-export const registerUser = async(req, res) => {
-    try {
-        const newUser = await userDao.registerUser(req.body);
-        if(newUser) res.redirect('/login');
-        else res.redirect('/error-register');
-    } catch (error) {
-        console.log(error);
+export default class UserController extends Controllers {
+    constructor() {
+        super(userService);
     }
-};
 
-export const loginUser = async(req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await userDao.loginUser(req.body);
-        if(user) {
-            req.session.email = email;
-            res.redirect('/profile');
-        } else res.redirect('/error-login')
-    } catch (error) {
-        console.log(error);
-    }
-};
+    register = async (req, res, next) => {
+        try {
+            const newUser = await userService.register(req.body);
+            if (!newUser) createResponse(res, 404, "User already exist");
+            else createResponse(res, 200, newUser);
+        } catch (error) {
+            next(error.message);
+        }
+    };
+
+    login = async (req, res, next) => {
+        try {
+            const token = await userService.login(req.body);
+            if (!token) createResponse(res, 404, "Error login");
+            else {
+                res.header("Authorization", token);
+                createResponse(res, 200, token);
+            }
+        } catch (error) {
+            next(error.message);
+        }
+    };
+
+    profile = (req, res, next) => {
+        try {
+            const { first_name, last_name, email, role } = req.user;
+            createResponse(res, 200, {
+                first_name,
+                last_name,
+                email,
+                role,
+            });
+        } catch (error) {
+            next(error.message);
+        }
+    };
+}
